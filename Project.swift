01a164b92304bb21d3058223219099058400f320
sentieant <aravindsn002@gmail.com>
2025-02-10 17:22:06 +0530

    my attempts to write project.swift file, still incomplete

--- Project.swift
+++ Project.swift
@@ -1,34 +1,47 @@
 import ProjectDescription
 
-let project = Project(
-    name: "tuist",
-    targets: [
-        .target(
-            name: "tuist",
-            destinations: .iOS,
-            product: .app,
-            bundleId: "io.tuist.tuist",
-            infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchScreen": [
-                        "UIColorName": "",
-                        "UIImageName": "",
-                    ],
-                ]
-            ),
-            sources: ["tuist/Sources/**"],
-            resources: ["tuist/Resources/**"],
-            dependencies: []
-        ),
-        .target(
-            name: "tuistTests",
-            destinations: .iOS,
-            product: .unitTests,
-            bundleId: "io.tuist.tuistTests",
-            infoPlist: .default,
-            sources: ["tuist/Tests/**"],
-            resources: [],
-            dependencies: [.target(name: "tuist")]
-        ),
+private let bundleId: String = "com.sentieant.ladybug"
+private let version: String = "1.0.0"
+private let bundleversion: String = "1"
+private let iOSTargetVersion: String = "16.0"
+
+private let basePath: String = "Target/ladybug"
+
+let project = Project(name: "ladybug",
+                      packages: [],
+                      settings: Settings.settings(configurations: makeConfigurations()),
+                      targets: [
+                        Target(
+                            name: "iOS",
+                            platform: .iOS,
+                            product: .app,
+                            bundleId: bundleId,
+                            deploymentTargets: .iOS(targetVersion: iOSTargetVersion, devices: .iphone),
+                            infoPlist: makeInfoPlist(),
+                            sources: ["\(basePath)/Sources/**"]
+                            resources: ["\(basePath)/Resources/**"]
+                            settings: baseSettings()
+                        )
+                      ],
+                      additionalFiles: [
+                        "README.md",
+                      ])
+
+private func makeInfoPlist(merging other: [String: InfoPlist: InfoPlist.Value] = [:] -> InfoPlist) {
+    var extendedPlist: [String: InfoPlist.Value] = [
+        "UIApplicationSceneManifest": ["UIApplicationSupportsMultipleScenes": true],
+        "UILaunchScreen": [],
+        "UISupportedInterfaceOrientation":
+            [
+                "UIInterfaceOrientationPortrait",
+            ],
+        "CFBundleShortVersionString": "\(version)",
+        "CFBundleVersion": "\(bundleversion)",
+        "CFBundleDisplayName": "ladybug"
     ]
-)
+    
+    other.forEach { (key: String, value: InfoPlist.Value) in
+        extendedPlist[key] = value
+        
+    }
+}
